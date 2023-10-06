428d220779a50b86ecede6300c763b4ca4cc7f38
Devitt3 <147190867+Devitt3@users.noreply.github.com>
2023-10-07 00:13:12 +0500

    Add files via upload

---
+++ zad1.java
@@ -0,0 +1,9 @@
+public class zad1 {
+    public static void main(String[] args) {
+        String name = "Даша";
+        int age = 17;
+        float weight = 59.7f;
+        System.out.printf("Name: %s Age: %d Weight: %.1f \n", name, age, weight);
+    }
+}
+
---
+++ zad2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class zad2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Введите второе число: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Введите оператор 1) +, 2) -, 3) *, 4) / ");
+        String operator = scanner.next();
+
+        double result;
+
+        switch (operator) {
+            case "1":
+                result = num1 + num2;
+                System.out.println("Результат: " + result);
+                break;
+            case "2":
+                result = num1 - num2;
+                System.out.println("Результат: " + result);
+                break;
+            case "3":
+                result = num1 * num2;
+                System.out.println("Результат: " + result);
+                break;
+            case "4":
+                if (num2 == 0) {
+                    System.out.println("Делить на ноль нельзя");
+                } else {
+                    result = num1 / num2;
+                    System.out.println("Результат: " + result);
+                }
+                break;
+            default:
+                System.out.println("Такого оператора нет");
+        }
+    }
+}
+
